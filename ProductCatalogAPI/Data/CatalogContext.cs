b25c4dc0680dd8c2a2f2cd46e04535e8d23482d8
jayarathna2019 <jayarathna.kanraj@gmail.com>
2019-07-28 10:50:35 -0700

    Adding Jewels on containers

---
+++ ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogBrand>(ConfigureCatalogBrand);
+            modelBuilder.Entity<CatalogType>(ConfigureCatalogType);
+            modelBuilder.Entity<CatalogItem>(ConfigureCatalogItem);
+        }
+        private void ConfigureCatalogBrand(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            builder.ToTable("CatalogBrands");
+            builder.Property(c => c.Id).IsRequired().ForSqlServerUseSequenceHiLo("Catalog_Brand_Hilo");
+            builder.property(c => c.brand).IsRequired().HasMaxLength(100);
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType>builder)
+        {
+           builder.ToTable("CatalogTypes");
+           builder.Property(c => c.Id).IsRequired().ForSqlServerUseSequenceHiLo("Catalog_Type_HiLo");
+           builder.property(c => c.type).IsRequired().HasMaxLength(100);
+        }
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+            builder.Property(c => c.Id).IsRequired().ForSqlServerUseSequenceHiLo("Catalog_HiLo");
+            builder.property(c => c.Name).IsRequired().HasMaxLength(50);
+            builder.property(c => c.price).IsRequired();
+            builder.HasOne(c => c.CatalogType).WithMany().HasForeignKey(c => c.CatalogTypeId);
+            builder.HasOne(c => c.CatalogBrand).WithMany().HasForeignKey(c => c.CatalogBrandId);
+        }
+    }
+}
---
+++ ProductCatalogAPI/Data/CatalogSeed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public static class CatalogSeed
+    {
+        public static void Seed(CatalogContext context)
+        {
+            if(!context.CatalogBrands.Any())
+            {
+                context.CatalogBrands.AddRange(GetPreConfiguredCatalogBrands());
+                context.SaveChanges();
+
+            }
+        }
+        private static IEnumerable<CatalogBrand>
+            GetPreConfiguredCatalogBrands()
+        {
+            return new List<CatalogBrand>()
+            {
+                new CatalogBrand() {Brand="Tiffany & co."},
+                new CatalogBrand() {Brand="DeBeers"},
+                new CatalogBrand() {Brand="Graff"}
+            }
+        }
+    }
+
+}
